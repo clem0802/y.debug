65c88876274d3004b56a43cd8bc8dc30a272189e
clem0802 <hhcc0802@gmail.com>
2022-06-07 17:30:50 +0200

    css

---
+++ script.js
@@ -0,0 +1,32 @@
+const operators = document.getElementById("operators");
+let currentOperator = ""
+
+operators.addEventListener('change', function (events) {
+    currentOperator = events.target.value;
+})
+
+function calculator() {
+    let firstNumber = parseInt(document.getElementById("firstNumber").value);
+    let secondNumber = parseInt(document.getElementById("secondNumber").value);
+
+    switch (currentOperator) {
+        case "+":
+            console.log(firstNumber + secondNumber)
+            break;
+        case "-":
+            console.log(firstNumber - secondNumber)
+            break;
+        case "*":
+            console.log(firstNumber * secondNumber)
+            break;
+        case "/":
+            console.log(firstNumber / secondNumber)
+            break;
+        case "%":
+            console.log(firstNumber % secondNumber)
+        break;
+        default:
+            console.log("Invalid Operator")
+            break;
+    }
+}
